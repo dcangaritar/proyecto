78fb82168444b627ba89127ef5393f1bf6ee595c
Dairo Angarita <dcangaritar@ufpso.edu.co>
2025-03-11 07:48:20 -0500

    ejercicios

---
+++ src/Tablademultiplicar.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tablademultiplicar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        scanner.close();
+    }
+}
